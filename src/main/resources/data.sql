35edd1bfa2cfaea38103c8a2dcddd63395685d8f
Dan Vega <danvega@gmail.com>
2020-06-13 11:19:25 -0400

    adding data and schema generation

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO book(id,title,author,publisher,release_date,isbn,topic)
+VALUES (1,'97 Things Every Java Programmer Should Know','Kevlin Henney, Trisha Gee','OReilly Media, Inc.','May 2020','9781491952696','Java');
+INSERT INTO book(id,title,author,publisher,release_date,isbn,topic)
+VALUES (2,'Spring Boot: Up and Running','Mark Heckler','OReilly Media, Inc.','February 2021',' 9781492076919','Spring');
+INSERT INTO book(id,title,author,publisher,release_date,isbn,topic)
+VALUES (3,'Hacking with Spring Boot 2.3: Reactive Edition','Greg L. Turnquist','Amazon.com Services LLC','May 2020','B086722L4L','Spring');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE book(
+    id INT NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    author VARCHAR(100) NOT NULL,
+    publisher VARCHAR(100),
+    release_date VARCHAR(20),
+    isbn VARCHAR(20),
+    topic VARCHAR(20)
+);
