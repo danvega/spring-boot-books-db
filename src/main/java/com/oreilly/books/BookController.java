41475f136bc6a54d0a8667a88703815321e3909d
Dan Vega <danvega@gmail.com>
2020-07-03 14:39:55 -0400

    removing code for starting code

--- src/main/java/com/oreilly/books/BookController.java
+++ src/main/java/com/oreilly/books/BookController.java
@@ -29,47 +29,4 @@ class BookController {
         return jdbcTemplate.queryForObject(sql, new Object[]{id}, new BookRowMapper());
     }
 
-    @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping
-    public void create(@RequestBody Book book) {
-        String sql = "insert into book(id,title,author,publisher,release_date,isbn,topic) values (?,?,?,?,?,?,?)";
-        int insert = jdbcTemplate.update(sql,
-                book.getId(),
-                book.getTitle(),
-                book.getAuthor(),
-                book.getPublisher(),
-                book.getReleaseDate(),
-                book.getIsbn(),
-                book.getTopic());
-
-        if( insert == 1 ) {
-            System.out.println("A new book was inserted into the datbase: " + book.getTitle());
-        }
-    }
-
-    @PutMapping("/{id}")
-    public void update(@RequestBody Book book, @PathVariable int id) {
-        String sql = "update book set title = ?, author = ?, publisher = ?, release_date = ?, isbn = ?, topic =? where id = ?";
-        int update = jdbcTemplate.update(sql,
-                book.getTitle(),
-                book.getAuthor(),
-                book.getPublisher(),
-                book.getReleaseDate(),
-                book.getIsbn(),
-                book.getTopic(),
-                id);
-        if( update == 1 ) {
-            System.out.println("Book: " + book.getTitle() + " was updated!");
-        }
-    }
-
-    @DeleteMapping("/{id}")
-    public void delete(@PathVariable int id) {
-        String sql = "delete from book where id = ?";
-        int delete = jdbcTemplate.update(sql,id);
-        if( delete == 0 ) {
-            System.out.println("Book: " + id + " was deleted.");
-        }
-    }
-
 }
