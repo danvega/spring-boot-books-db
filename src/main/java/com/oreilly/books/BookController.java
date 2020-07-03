6907c8a0ea3d7bb6bf4dea6b495ced0a47431761
Dan Vega <danvega@gmail.com>
2020-07-03 14:38:34 -0400

    finishing up scenario 4

--- src/main/java/com/oreilly/books/BookController.java
+++ src/main/java/com/oreilly/books/BookController.java
@@ -1,10 +1,8 @@
 package com.oreilly.books;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,4 +28,48 @@ class BookController {
         String sql = "SELECT * FROM BOOK WHERE ID = ?";
         return jdbcTemplate.queryForObject(sql, new Object[]{id}, new BookRowMapper());
     }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public void create(@RequestBody Book book) {
+        String sql = "insert into book(id,title,author,publisher,release_date,isbn,topic) values (?,?,?,?,?,?,?)";
+        int insert = jdbcTemplate.update(sql,
+                book.getId(),
+                book.getTitle(),
+                book.getAuthor(),
+                book.getPublisher(),
+                book.getReleaseDate(),
+                book.getIsbn(),
+                book.getTopic());
+
+        if( insert == 1 ) {
+            System.out.println("A new book was inserted into the datbase: " + book.getTitle());
+        }
+    }
+
+    @PutMapping("/{id}")
+    public void update(@RequestBody Book book, @PathVariable int id) {
+        String sql = "update book set title = ?, author = ?, publisher = ?, release_date = ?, isbn = ?, topic =? where id = ?";
+        int update = jdbcTemplate.update(sql,
+                book.getTitle(),
+                book.getAuthor(),
+                book.getPublisher(),
+                book.getReleaseDate(),
+                book.getIsbn(),
+                book.getTopic(),
+                id);
+        if( update == 1 ) {
+            System.out.println("Book: " + book.getTitle() + " was updated!");
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable int id) {
+        String sql = "delete from book where id = ?";
+        int delete = jdbcTemplate.update(sql,id);
+        if( delete == 0 ) {
+            System.out.println("Book: " + id + " was deleted.");
+        }
+    }
+
 }
