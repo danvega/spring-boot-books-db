53b32d129cece3b5a09b2ba8dfa284983183eae0
Dan Vega <danvega@gmail.com>
2020-07-04 05:53:29 -0400

    moving jdbc operations to service class

--- src/main/java/com/oreilly/books/BookService.java
+++ src/main/java/com/oreilly/books/BookService.java
@@ -1,7 +1,16 @@
 package com.oreilly.books;
 
+import java.util.List;
+
 public interface BookService {
+
+    List<Book> list();
+
+    Book get(int id);
 
     void create(Book book);
 
+    void update(Book book, int id);
+
+    void delete(int id);
 }
